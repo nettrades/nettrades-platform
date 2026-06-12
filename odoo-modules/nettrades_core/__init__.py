c11b9d51a15caf75fb5331af71aa9d9e2a536764
nettrades <jamil.aslam@gmail.com>
2026-06-12 18:47:56 +0100

    fixing errors in nettrades_core

--- odoo-modules/nettrades_core/__init__.py
+++ odoo-modules/nettrades_core/__init__.py
@@ -2,5 +2,5 @@
 # Section: A-F (NETTRADES CORE)
 # Purpose: Initialisation file for the NETTRADES Core module.
 # This file makes the directory a Python package recognised by Odoo.
-from . import models
-from . import controllers
+# from . import models
+# from . import controllers
