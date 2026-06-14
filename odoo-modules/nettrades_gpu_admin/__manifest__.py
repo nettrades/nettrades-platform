d8a213aeda0bc01bb437336eff5b423734d4290c
nettrades <jamil.aslam@gmail.com>
2026-06-15 00:53:23 +0100

    <tree> is depreceated in Odoo 19, ensured these XML files in Odoo 19 are using <list> instead of <tree>

--- odoo-modules/nettrades_gpu_admin/__manifest__.py
+++ odoo-modules/nettrades_gpu_admin/__manifest__.py
@@ -3,30 +3,30 @@
 {
     'name': 'NETTRADES GPU Administration Panel',
     'version': '1.0',
-#   'category': 'Tools',
-#   'summary': 'Network administrator dashboard for managing GPU clusters',
-    'depends': ['base'],#, 'web', 'bus', 'nettrades_core'
-#    'data': [
-#        'security/ir.model.access.csv',
-#        'security/gpu_admin_security.xml',
-#        'views/gpu_cluster_views.xml',
-#        'views/gpu_node_views.xml',
-#        'views/gpu_schedule_views.xml',
-#        'views/gpu_token_economics_views.xml',
-#        'views/gpu_dashboard_templates.xml',
-#        'views/menu_items.xml',
-#        'views/multimodal_config_views.xml',   # new
-#        'data/cron.xml',
-#   ],
-#    'assets': {
-#       'web.assets_backend': [
-#            'nettrades_gpu_admin/static/src/js/dashboard.js',
-#            'nettrades_gpu_admin/static/src/js/node_manager.js',
-#            'nettrades_gpu_admin/static/src/js/network_scan.js',
-#            'nettrades_gpu_admin/static/src/js/wireguard_manager.js',
-#            'nettrades_gpu_admin/static/src/scss/dashboard.scss',
-#        ],
-#    },
+   'category': 'Tools',
+   'summary': 'Network administrator dashboard for managing GPU clusters',
+    'depends': ['base', 'web', 'bus', 'nettrades_core'],
+    'data': [
+        'security/ir.model.access.csv',
+        'security/gpu_admin_security.xml',
+        'views/gpu_cluster_views.xml',
+        'views/gpu_node_views.xml',
+        'views/gpu_schedule_views.xml',
+        'views/gpu_token_economics_views.xml',
+        'views/gpu_dashboard_templates.xml',
+        'views/menu_items.xml',
+        'views/multimodal_config_views.xml',   # new
+        'data/cron.xml',
+   ],
+    'assets': {
+       'web.assets_backend': [
+            'nettrades_gpu_admin/static/src/js/dashboard.js',
+            'nettrades_gpu_admin/static/src/js/node_manager.js',
+            'nettrades_gpu_admin/static/src/js/network_scan.js',
+            'nettrades_gpu_admin/static/src/js/wireguard_manager.js',
+            'nettrades_gpu_admin/static/src/scss/dashboard.scss',
+        ],
+    },
     'controllers': ['controllers/main.py'],
     'installable': True,
     'application': True,
