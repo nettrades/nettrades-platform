78f03312f4ab51c06d2a8c0f78f5117493650c04
nettrades <jamil.aslam@gmail.com>
2026-06-14 04:25:37 +0100

    bug fixes

--- odoo-modules/nettrades_gpu_admin/models/__init__.py
+++ odoo-modules/nettrades_gpu_admin/models/__init__.py
@@ -3,8 +3,8 @@ from . import test                    # dummy, safe
 
 # Models that are referenced by others must come first
 from . import gpu_cluster             # defines gpu.cluster
-from . import gpu_cluster_subnet      # references gpu.cluster
-from . import gpu_node                # references gpu.cluster
-from . import gpu_sharing_schedule    # references gpu.cluster
-from . import gpu_token_economics     # references gpu.cluster (if any)
-from . import multimodal_config       # simple config, no dependencies
+#from . import gpu_cluster_subnet      # references gpu.cluster
+#from . import gpu_node                # references gpu.cluster
+#from . import gpu_sharing_schedule    # references gpu.cluster
+#from . import gpu_token_economics     # references gpu.cluster (if any)
+#from . import multimodal_config       # simple config, no dependencies
